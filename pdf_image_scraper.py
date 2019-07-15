7acc869b57d8a80523566b98761b1c2ba1b36ffb
Odinukweze-Bruno <52933956+Odinukweze-Bruno@users.noreply.github.com>
2019-07-15 20:25:47 +0100

    Added the PDF_Image_scraper
    This engines Extracts images from pdf files and save them to a specified directory

---
+++ pdf_image_scraper.py
@@ -0,0 +1,16 @@
+#import neccesary packages
+import fitz  
+
+doc = "OBINNA.pdf" # path to pdf file
+doc = fitz.open(doc)
+pno = doc.loadPage(1)
+text = pno.getText('dict')# dict format of the file
+blocks = text["blocks"]
+imgblocks = [b for b in blocks if b["type"] == 1]
+textblocks = [t for t in blocks if t["type"] == 0]
+
+
+for index, img in enumerate(imgblocks):
+    img_name = "obinna_img/%s-%s.%s" % (doc, index, img['ext'])
+    with open(img_name, 'wb') as f:
+        f.write(img['image'])
